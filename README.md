542eb4d8e6786439560311f905fe3edc3e8e21c1
filbert <fillberth@gmail.com>
2020-10-02 15:23:18 +0800

    created timeline and basic styling

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # filbertphang.github.io
 WIP: filbert's resume site
+
+https://coolors.co/003d75-005fb8-ffffeb-ffdcd6-ffadad
---
+++ assets/js/timeline.js
@@ -0,0 +1,3 @@
+var timeline = document.getElementsByClassName("timeline")[0]
+parent_height = document.getElementsByClassName("timeline-container")[0].scrollHeight - 32
+timeline.style.height = parent_height + "px";
